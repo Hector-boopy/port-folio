6f2260914d4a36e366935c0716007bf271dfd528
PFISTER Hector <hectorpfister@hotmail.fr>
2025-10-06 00:03:47 +0200

    started working on the title, still need work

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Projects } from './projects/projects';
+import { Title } from './title/title';
+import { About } from './about/about';
+import { Contact } from './contact/contact';
+import { Navbar } from './navbar/navbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Projects],
+  imports: [RouterOutlet, Projects, Title, About, Contact, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
